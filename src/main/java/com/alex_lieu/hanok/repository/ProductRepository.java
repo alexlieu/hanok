060714497039c2a8d43562b922cd299ff1cdc91d
alexlieu <alieu2001@googlemail.com>
2025-03-13 23:39:08 +0000

    chore(repository): Remove unused methods and change return types to not use Page
    Dealing with Pageable objects and Page objects was too complex for the
    scale of this project considering they weren't necessary.
    I think it best to start with the Service classes first to see what
    repository methods I require.

--- src/main/java/com/alex_lieu/hanok/repository/ProductRepository.java
+++ src/main/java/com/alex_lieu/hanok/repository/ProductRepository.java
@@ -7,25 +7,27 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Page;
-
 import java.math.BigDecimal;
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    // This excludes any Products without Variants so it needs to be ensured that when a Product is made, a variant is made also.
     @Query("SELECT DISTINCT p FROM Product p JOIN p.variations v " +
             "WHERE (:category IS NULL OR p.category = :category) " +
             "AND (:name IS NULL OR LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%'))) " +
             "AND (:minPrice IS NULL OR v.price >= :minPrice) " +
             "AND (:maxPrice IS NULL OR v.price <= :maxPrice) " +
-            "AND (:available IS NULL OR v.available = :available)")
-    Page<Product> searchProducts(
+            "AND (:available IS NULL OR v.available = :available)" +
+            "AND p.active = :active"
+    )
+    List<Product> searchProducts(
             @Param("category") Category category,
             @Param("name") String name,
             @Param("minPrice") BigDecimal minPrice,
             @Param("maxPrice") BigDecimal maxPrice,
             @Param("available") Boolean available,
-            Pageable pageable);
+            @Param("active") Boolean isActive
+    );
 
 }
