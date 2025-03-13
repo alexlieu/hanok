0e4e26dca36bd6d12bb4074ca07605a8f08b9632
alexlieu <alieu2001@googlemail.com>
2025-03-13 23:39:08 +0000

    chore(repository): Remove unused methods and change return types to not use Page
    Dealing with Pageable objects and Page objects was too complex for the
    scale of this project considering they weren't necessary.
    I think it best to start with the Service classes first to see what
    repository methods I require.

--- src/main/java/com/alex_lieu/hanok/repository/CustomerOrderRepository.java
+++ src/main/java/com/alex_lieu/hanok/repository/CustomerOrderRepository.java
@@ -1,17 +1,13 @@
 package com.alex_lieu.hanok.repository;
 import com.alex_lieu.hanok.model.CustomerOrder;
-import com.alex_lieu.hanok.model.Person;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import java.time.LocalDateTime;
 
 @Repository
 public interface CustomerOrderRepository extends JpaRepository<CustomerOrder, Long> {
-    Page<CustomerOrder> findByCustomerId(Long id, Pageable pageable);
-    Page<CustomerOrder> findByOrderStatus(CustomerOrder.OrderStatus orderStatus, Pageable pageable);
-    Page<CustomerOrder> findByPickupDateTimeBetween(LocalDateTime startDateTime, LocalDateTime endDateTime, Pageable pageable);
+    Page<CustomerOrder> findOrderItemsByCustomerId(long id, Pageable pageable);
 }
