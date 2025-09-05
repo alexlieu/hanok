1cfe17bff424cb777af809836ab00622f7738389
alexlieu <alieu2001@googlemail.com>
2025-09-05 23:07:41 +0100

    chore(frontend/PickupMap): Update MapControls to use the custom Button component, update their styles, and add disabled states for all map controls

--- frontend/src/components/checkout/PickupMap.tsx
+++ frontend/src/components/checkout/PickupMap.tsx
@@ -1,7 +1,10 @@
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import { AiOutlineShop } from "react-icons/ai";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import L from "leaflet";
+import { Button } from "../ui/aria/Button";
+import { LuPlus, LuMinus, LuCakeSlice } from "react-icons/lu";
+import { tv } from "tailwind-variants";
 
 const position: [number, number] = [51.40313097396538, -0.2730678337183401];
 const maxZoom = 18;
@@ -11,77 +14,74 @@ const PickupMap: React.FC = () => {
   function MapControls() {
     const map = useMap();
     const [currentZoom, setCurrentZoom] = useState(map.getZoom());
+    const [center, setCenter] = useState(map.getCenter());
+    const controlsRef = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+      if (controlsRef.current) {
+        L.DomEvent.disableClickPropagation(controlsRef.current);
+        L.DomEvent.disableScrollPropagation(controlsRef.current);
+      }
+    }, []);
 
     useEffect(() => {
-      const updateZoom = () => {
+      const updateMapState = () => {
+        setCenter(map.getCenter());
         setCurrentZoom(map.getZoom());
       };
 
-      map.on("zoomend", updateZoom);
+      map.on("moveend", updateMapState);
+      map.on("zoomend", updateMapState);
 
       return () => {
-        map.off("zoomend", updateZoom);
+        map.off("moveend", updateMapState);
+        map.off("zoomend", updateMapState);
       };
     }, [map]);
 
     const canZoomOut = currentZoom - 1 >= minZoom;
     const canZoomIn = currentZoom + 1 <= maxZoom;
+    const tolerance = 0.00001;
+    const atOrigin =
+      currentZoom === maxZoom && center.equals(position, tolerance);
+
+    const buttonStyling = tv({
+      base: "w-4/5 bg-brand-colour-2/70 backdrop-blur-xs text-default-bg hover:bg-brand-colour-5/40 hover:text-black p-1 focus:outline-brand-colour-4",
+      variants: {
+        isDisabled: {
+          true: "text-brand-colour-2 pressed:scale-100 hover:text-brand-colour-2",
+        },
+      },
+    });
 
     return (
-      <div className="absolute top-0 right-0 w-15 h-35 gap-2 z-1000 flex flex-col justify-center items-center">
-        <button
-          type="button"
-          className="w-1/2 aspect-square bg-stone-200 border-2 border-stone-900 group"
-          onClick={() => {
-            map.setView(position, maxZoom, { animate: true });
-            // map.flyTo(position, maxZoom, { duration: 1, easeLinearity: 0.5 });
-          }}
+      <div
+        ref={controlsRef}
+        className="absolute top-2 right-2 w-10 h-30 z-1000 flex flex-col justify-evenly items-center"
+      >
+        <Button
+          variant="icon"
+          className={buttonStyling}
+          onClick={() => map.setView(position, maxZoom, { animate: true })}
+          isDisabled={atOrigin}
         >
-          <AiOutlineShop className="size-full" />
-        </button>
-        <button
-          type="button"
-          className={`relative w-1/2 aspect-square bg-stone-200 border-2 border-stone-900 group`}
-          onClick={() => {
-            map.setZoom(map.getZoom() + 1);
-          }}
+          <LuCakeSlice className="size-full" strokeWidth={2} />
+        </Button>
+        <Button
+          variant="icon"
+          className={buttonStyling}
+          onClick={() => map.setZoom(map.getZoom() + 1)}
+          isDisabled={!canZoomIn}
         >
-          <div
-            className={`
-                before:content-[''] before:absolute
-                before:w-3/5 before:h-[3px]
-                before:rounded-md
-                before:left-1/2 before:top-1/2 before:-translate-x-1/2 before:-translate-y-1/2
-                before:transform before:transition-colors before:duration-250
-
-                after:content-[''] after:absolute
-                after:w-3/5 after:h-[3px]
-                after:rounded-md
-                after:left-1/2 after:top-1/2 after:-translate-x-1/2 after:-translate-y-1/2
-                after:transform after:rotate-90 after:transition-colors after:duration-250
-
-                ${
-                  canZoomIn
-                    ? "before:bg-black after:bg-black"
-                    : "before:bg-gray-400 after:bg-gray-400"
-                }
-
-              `}
-          ></div>
-        </button>
-        <button
-          type="button"
-          className="flex items-center justify-center w-1/2 aspect-square bg-stone-200 border-2 border-stone-900 group"
-          onClick={() => {
-            map.setZoom(map.getZoom() - 1);
-          }}
+          <LuPlus className="size-full" strokeWidth={2} />
+        </Button>
+        <Button
+          variant="icon"
+          className={buttonStyling}
+          onClick={() => map.setZoom(map.getZoom() - 1)}
+          isDisabled={!canZoomOut}
         >
-          <div
-            className={`w-3/5 h-[3px] transition-colors duration-250 ${
-              canZoomOut ? "bg-black" : "bg-gray-400"
-            }`}
-          ></div>
-        </button>
+          <LuMinus className="size-full" strokeWidth={2} />
+        </Button>
       </div>
     );
   }
