e029b6501aef3763fa7fd8763df0147654541ec2
alexlieu <alieu2001@googlemail.com>
2025-10-19 16:47:23 +0100

    refactor(frontend/Checkbox): Remove scaling animation
    The scaling animation was inconsistent across browsers - it would appear
    jittery - so it was removed.
    The focus visible styling also seemed to be inconsistent - sometimes not
    appearing - which may have been a result of a styling conflict with a
    motion component. The focus styling is applied to a vanilla html element
    instead to resolve this.
    
    - Add hover styling where an area around the Checkbox is highlighted via
    background colour change. The ring-offset colour is changed when the
    Checkbox is hovered and is focus-visible to maintain this styling change

--- frontend/src/components/ui/aria/Button.tsx
+++ frontend/src/components/ui/aria/Button.tsx
@@ -25,7 +25,7 @@ const button = tv({
         "text-black bg-transparent enabled:hover:bg-black enabled:hover:text-default-bg enabled:hover:border-black",
       icon: "border-0 p-1 flex items-center justify-center text-brand-colour-5 hover:bg-icon-hover hover:text-default-bg pressed:scale-90 pressed:transition-transform",
       iconNoInteraction:
-        "border-0 p-1 flex items-center justify-center focus-visible:ring-offset-0",
+        "border-0 p-1 flex items-center justify-center focus-visible:ring-offset-1",
     },
     isDisabled: {
       true: "",
--- frontend/src/components/ui/aria/Checkbox.tsx
+++ frontend/src/components/ui/aria/Checkbox.tsx
@@ -13,7 +13,7 @@ import { createLabel } from "./utils/createLabel";
 import { LuMinus as Minus } from "react-icons/lu";
 import { CheckIcon as Check } from "../icons/Check";
 import { RefCallBack } from "react-hook-form";
-import { motion, Transition } from "motion/react";
+import { motion } from "motion/react";
 import { ClickBurstIcon } from "../icons/ClickBurst";
 
 export interface CheckboxGroupProps
@@ -64,7 +64,7 @@ export function CheckboxGroup({
 }
 
 const checkboxStyles = tv({
-  base: "flex gap-2 items-center text-sm transition relative",
+  base: "flex gap-2 items-center text-sm transition relative outline-hidden",
   variants: {
     isDisabled: {
       false: "text-gray-800",
@@ -75,10 +75,9 @@ const checkboxStyles = tv({
 
 const boxStyles = tv({
   extend: focusRing,
-  base: "w-[1.5rem] h-[1.5rem] shrink-0 flex items-center justify-center border-2 border-brand-colour-5 transition",
+  base: "w-[1.5rem] h-[1.5rem] shrink-0 flex items-center justify-center border-2 border-brand-colour-5 transition relative",
   variants: {
     isSelected: {
-      false: "",
       true: "",
     },
     isInvalid: {
@@ -91,14 +90,7 @@ const boxStyles = tv({
 });
 
 const iconStyles =
-  "w-4 h-4 text-brand-colour-5 group-disabled:text-unavailable-text";
-
-const pressedScale = 0.9;
-const scaleTransition: Transition = {
-  type: "spring",
-  stiffness: 400,
-  damping: 17,
-};
+  "w-4 h-4 text-brand-colour-5 group-disabled:text-unavailable-text absolute";
 
 export function Checkbox(props: CheckboxProps) {
   return (
@@ -109,68 +101,71 @@ export function Checkbox(props: CheckboxProps) {
         checkboxStyles({ ...renderProps, className })
       )}
     >
-      {({ isSelected, isIndeterminate, isPressed, ...renderProps }) => {
-        const playSelectAnimation = !isSelected && isPressed;
+      {({
+        isSelected,
+        isIndeterminate,
+        isFocusVisible,
+        isHovered,
+        ...renderProps
+      }) => {
         return (
           <>
-            <div className="relative" aria-hidden>
+            <motion.div
+              initial="normal"
+              animate={isHovered ? "hover" : "normal"}
+              variants={{
+                normal: {
+                  backgroundColor: "var(--color-default-bg)",
+                },
+                hover: {
+                  backgroundColor: "var(--color-checkbox-hover-bg)",
+                },
+              }}
+              transition={{
+                type: "spring",
+                stiffness: 400,
+                damping: 30,
+              }}
+              className="p-1"
+            >
               <div
-                className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 pointer-events-none"
-                aria-hidden
-              >
-                <ClickBurstIcon
-                  playAnimation={isSelected}
-                  size="3rem"
-                  stroke="var(--color-brand-colour-1)"
-                  strokeWidth={2}
-                  aria-hidden
-                />
-              </div>
-              <motion.div
-                variants={{
-                  normal: {
-                    scale: 1,
-                  },
-                  pressed: {
-                    scale: pressedScale,
-                    transition: scaleTransition,
-                  },
-                }}
-                initial="normal"
-                animate={playSelectAnimation ? "pressed" : "normal"}
                 className={boxStyles({
+                  isFocusVisible: isFocusVisible,
                   isSelected: isSelected || isIndeterminate,
+                  className: `${
+                    isHovered &&
+                    isFocusVisible &&
+                    "ring-offset-[var(--color-checkbox-hover-bg)]"
+                  }`,
                   ...renderProps,
                 })}
                 aria-hidden
               >
-                <motion.div
-                  variants={{
-                    normal: {
-                      scale: 1,
-                    },
-                    pressed: {
-                      scale: 1 / pressedScale,
-                    },
-                  }}
-                  initial="normal"
-                  animate={playSelectAnimation ? "pressed" : "normal"}
-                  transition={scaleTransition}
+                <div
+                  className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 pointer-events-none"
                   aria-hidden
                 >
-                  {isIndeterminate ? (
-                    <Minus aria-hidden className={iconStyles} />
-                  ) : (
-                    <Check
-                      playAnimation={isSelected}
-                      size={"1.25rem"}
-                      stroke="var(--color-brand-colour-5)"
-                      aria-hidden
-                    />
-                  )}
-                </motion.div>
-              </motion.div>
-            </div>
+                  <ClickBurstIcon
+                    playAnimation={isSelected}
+                    size="3rem"
+                    stroke="var(--color-brand-colour-1)"
+                    strokeWidth={2}
+                    aria-hidden
+                  />
+                </div>
+                {isIndeterminate ? (
+                  <Minus aria-hidden className={iconStyles} />
+                ) : (
+                  <Check
+                    playAnimation={isSelected}
+                    size={"1.25rem"}
+                    stroke="var(--color-brand-colour-5)"
+                    className={"absolute"}
+                    aria-hidden
+                  />
+                )}
+              </div>
+            </motion.div>
             {props.children}
           </>
         );
