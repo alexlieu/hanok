1c77abffc79bf2e671b14023636c5a60fb59f63d
alexlieu <alieu2001@googlemail.com>
2025-11-06 17:53:25 +0000

    feat(frontend/checkout): Update back to basket chevron animation and style

--- frontend/src/pages/Checkout.tsx
+++ frontend/src/pages/Checkout.tsx
@@ -3,7 +3,8 @@ import { useMediaQuery } from "../utils/hooks/useWindowDimensions";
 import { CheckoutRequiredData } from "../types/CheckoutType";
 import { CheckoutSession } from "../components/checkout/CheckoutSession";
 import { Button } from "../components/ui/aria/Button";
-import { FaChevronLeft } from "react-icons/fa";
+import { ChevronBack } from "../components/ui/icons/ChevronBack";
+import { motion } from "motion/react";
 
 const CheckoutPage: React.FC = () => {
   const navigate = useNavigate();
@@ -19,15 +20,17 @@ const CheckoutPage: React.FC = () => {
           <div className="mx-auto">
             <div className="flex items-center justify-between h-[4.5rem] relative">
               <Button
-                className="flex gap-1 text-[0.8rem] group"
                 variant="icon"
                 onClick={() => navigate("/basket")}
+                className="p-0"
               >
-                <FaChevronLeft
-                  size="0.8rem"
-                  className="group-hover:translate-x-[-0.15rem] delay-150 ease-out transition-transform"
-                />
-                <span>back to basket</span>
+                <motion.div
+                  whileHover="hover"
+                  className="flex gap-1 text-[0.8rem] items-center p-1"
+                >
+                  <ChevronBack size="0.8rem" strokeWidth={"1.8px"} />
+                  <span>back to basket</span>
+                </motion.div>
               </Button>
               <h1 className="text-3xl absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2">
                 checkout
