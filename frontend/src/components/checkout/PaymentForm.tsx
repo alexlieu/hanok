2823b26cfc943777fb7618f96706393a3e041f75
alexlieu <alieu2001@googlemail.com>
2025-11-07 20:40:46 +0000

    fix(frontend/checkout): Improve morphing effect in DisclosureRadioGroup animation
    Before, the disclosures were separated using a margin. Any margin
    updates were not animated which wasn't effective in creating the
    morphing effect. However, when margin is animated it leads to unwanted
    layout shifts in content above the DisclosureRadioGroup. Therefore, the
    margin animation was replaced in favour of a translation animation which
    does not affect the layout of surrounding DOM elements.

--- frontend/src/components/checkout/PaymentForm.tsx
+++ frontend/src/components/checkout/PaymentForm.tsx
@@ -14,109 +14,100 @@ import Amex from "../../assets/checkout_logos/amex.svg?react";
 import Paypal from "../../assets/checkout_logos/payment-types/paypal-logo-alternative.svg?react";
 import Apple from "../../assets/checkout_logos/payment-types/apple-pay-logo.svg?react";
 import Google from "../../assets/checkout_logos/payment-types/google-pay-logo.svg?react";
-import { LayoutGroup, motion, Transition, Variants } from "motion/react";
+import { motion, Transition, Variants } from "motion/react";
 import { getIssuingBank } from "../../schemas/CardSchema";
 import { allLogos, getActiveCards } from "../../utils/cardUtils";
 import { tv } from "tailwind-variants";
 import TokenPaymentIcon from "../ui/icons/TokenPaymentIcon";
 import { useWindowDimensions } from "../../utils/hooks/useWindowDimensions";
-import { WINDOW_BOUNDARIES } from "../../constants/windowBoundaries";
+
+const borderRadiusTransition: Transition = {
+  type: "spring",
+  stiffness: 400,
+  damping: 20,
+  mass: 1.7,
+};
+const layoutTransition: Transition = {
+  y: {
+    type: "spring",
+    stiffness: 300,
+    damping: 10,
+    mass: 0.9,
+  },
+  borderTopLeftRadius: borderRadiusTransition,
+  borderTopRightRadius: borderRadiusTransition,
+  borderBottomRightRadius: borderRadiusTransition,
+  borderBottomLeftRadius: borderRadiusTransition,
+};
 
 const panelTransition: Transition = {
-  default: {
-    type: "spring",
-    stiffness: 400,
-    damping: 25,
-    mass: 1.7,
-    delay: 0.05,
-  },
+  ease: "easeOut",
+  duration: 0.3,
 };
 
-// Helper function to get animated border styles (similar to Tailwind Variants)
-const getAnimatedBorderStyle = ({
-  isExpanded,
-  isAboveExpanded,
-  isBelowExpanded,
-  isTop,
-  isBottom,
-  windowWidth,
+const BORDER_WIDTH = 2;
+
+const getAnimationStyles = ({
+  index,
+  expandedIndex,
 }: {
-  isExpanded: boolean;
-  isAboveExpanded: boolean;
-  isBelowExpanded: boolean;
-  isTop: boolean;
-  isBottom: boolean;
-  windowWidth: number;
+  index: number;
+  expandedIndex: number;
 }) => {
+  const GAP = 13;
+  const isExpanded = index === expandedIndex;
+  const isBefore = index < expandedIndex;
+  const isAfter = index > expandedIndex;
+  const isFirstAbove = expandedIndex !== -1 && index === expandedIndex - 1;
+  const isFirstBelow = expandedIndex !== -1 && index === expandedIndex + 1;
+  const isTop = index === 0;
+  const isBottom = index === PAYMENT_METHODS.length - 1;
+
   const roundedVal = "0.5rem";
   const defaultBackgroundColor = "#e8e8e8";
-  const noTranslation = windowWidth < WINDOW_BOUNDARIES.LARGE;
+
+  let borderTopLeftRadius = "0px";
+  let borderTopRightRadius = "0px";
+  let borderBottomRightRadius = "0px";
+  let borderBottomLeftRadius = "0px";
 
   if (isExpanded) {
-    return {
-      borderTopLeftRadius: roundedVal,
-      borderTopRightRadius: roundedVal,
-      borderBottomRightRadius: roundedVal,
-      borderBottomLeftRadius: roundedVal,
-      backgroundColor: defaultBackgroundColor,
-      x: noTranslation ? 0 : 5,
-    };
+    borderTopLeftRadius = roundedVal;
+    borderTopRightRadius = roundedVal;
+    borderBottomRightRadius = roundedVal;
+    borderBottomLeftRadius = roundedVal;
+  } else if ((isTop && isFirstAbove) || (isBottom && isFirstBelow)) {
+    borderTopLeftRadius = roundedVal;
+    borderTopRightRadius = roundedVal;
+    borderBottomRightRadius = roundedVal;
+    borderBottomLeftRadius = roundedVal;
+  } else if (isTop || isFirstBelow) {
+    borderTopLeftRadius = roundedVal;
+    borderTopRightRadius = roundedVal;
+  } else if (isBottom || isFirstAbove) {
+    borderBottomRightRadius = roundedVal;
+    borderBottomLeftRadius = roundedVal;
   }
 
-  let borderTopLeft = "0px";
-  let borderTopRight = "0px";
-  let borderBottomRight = "0px";
-  let borderBottomLeft = "0px";
-
-  if ((isTop && isAboveExpanded) || (isBottom && isBelowExpanded)) {
-    borderTopLeft = roundedVal;
-    borderTopRight = roundedVal;
-    borderBottomRight = roundedVal;
-    borderBottomLeft = roundedVal;
-  } else if (isTop || isBelowExpanded) {
-    borderTopLeft = roundedVal;
-    borderTopRight = roundedVal;
-  } else if (isBottom || isAboveExpanded) {
-    borderBottomRight = roundedVal;
-    borderBottomLeft = roundedVal;
-  }
+  const y = index * -BORDER_WIDTH;
 
   return {
-    borderTopLeftRadius: borderTopLeft,
-    borderTopRightRadius: borderTopRight,
-    borderBottomRightRadius: borderBottomRight,
-    borderBottomLeftRadius: borderBottomLeft,
+    borderTopLeftRadius,
+    borderTopRightRadius,
+    borderBottomRightRadius,
+    borderBottomLeftRadius,
     backgroundColor: defaultBackgroundColor,
-    x: 0,
-  };
-};
-
-const getMarginStyles = ({
-  isExpanded,
-  isAboveExpanded,
-  isBelowExpanded,
-  isTop,
-  isBottom,
-}: {
-  isExpanded: boolean;
-  isAboveExpanded: boolean;
-  isBelowExpanded: boolean;
-  isTop: boolean;
-  isBottom: boolean;
-}) => {
-  if (isExpanded) {
-    return {
-      marginTop: isTop ? 0 : 10,
-      marginBottom: isBottom ? 0 : 10,
-    };
-  }
-  const showTop = isTop || isBelowExpanded;
-  const showBottom = isBottom || isAboveExpanded;
-  const marginTop = showTop ? 0 : -2;
-  const marginBottom = showBottom ? 0 : -2;
-  return {
-    marginTop,
-    marginBottom,
+    y: isBefore
+      ? y
+      : isExpanded
+      ? isTop
+        ? 0
+        : y + GAP
+      : isAfter
+      ? expandedIndex === 0
+        ? y + GAP
+        : y + GAP * 2
+      : 0,
   };
 };
 
@@ -160,144 +151,124 @@ const PaymentForm = () => {
         name="paymentMethod"
         control={control}
         render={({ field: { onChange, value, ref } }) => (
-          <LayoutGroup id="payment-methods-group">
-            <DisclosureRadioGroup
-              onChange={onChange}
-              value={value}
-              isRequired={true}
-              aria-label="Payment Method"
-              inputRef={ref}
-            >
-              {PAYMENT_METHODS.map(({ value, label }, index) => {
-                const isExpanded = expandedIndex === index;
-                const isAboveExpanded =
-                  expandedIndex !== -1 && index === expandedIndex - 1;
-                const isBelowExpanded =
-                  expandedIndex !== -1 && index === expandedIndex + 1;
-                const isTop = index === 0;
-                const isBottom = index === PAYMENT_METHODS.length - 1;
-                const borderStyle = getAnimatedBorderStyle({
-                  isExpanded,
-                  isAboveExpanded,
-                  isBelowExpanded,
-                  isTop,
-                  isBottom,
-                  windowWidth,
-                });
-                const marginStyles = getMarginStyles({
-                  isExpanded,
-                  isAboveExpanded,
-                  isBelowExpanded,
-                  isTop,
-                  isBottom,
-                });
-                return (
-                  <motion.div
-                    key={value}
-                    initial={false}
-                    layout="position"
-                    layoutDependency={expandedIndex}
-                    animate={borderStyle}
-                    transition={panelTransition}
-                    style={marginStyles}
+          <DisclosureRadioGroup
+            onChange={onChange}
+            value={value}
+            isRequired={true}
+            aria-label="Payment Method"
+            inputRef={ref}
+          >
+            {PAYMENT_METHODS.map(({ value, label }, index) => {
+              const isExpanded = expandedIndex === index;
+              const borderStyle = getAnimationStyles({
+                index,
+                expandedIndex,
+              });
+              return (
+                <motion.div
+                  key={value}
+                  initial={false}
+                  layout="position"
+                  layoutDependency={expandedIndex}
+                  animate={borderStyle}
+                  transition={layoutTransition}
+                >
+                  <DisclosureRadio
+                    value={value}
+                    panelTransition={panelTransition}
+                    panelContent={
+                      value === "card" ? (
+                        <div className="px-5 pt-3 pb-7 space-y-6">
+                          <CardDetailsForm
+                            issuingBank={issuingBank}
+                            activeCards={activeCards}
+                          />
+                          <BillingAddressForm />
+                        </div>
+                      ) : (
+                        <div className="py-3">
+                          <TokenPaymentIcon
+                            className={`m-auto ${
+                              windowWidth < 1023 && windowWidth > 767
+                                ? "-scale-x-100 w-1/2"
+                                : "w-1/3"
+                            }`}
+                          />
+                          <p className="text-center mt-4">
+                            Click "Pay with {value}" to complete your purchase.
+                          </p>
+                        </div>
+                      )
+                    }
                   >
-                    <DisclosureRadio
-                      value={value}
-                      panelContent={
-                        value === "card" ? (
-                          <div className="px-5 pt-3 pb-7 space-y-6">
-                            <CardDetailsForm
-                              issuingBank={issuingBank}
-                              activeCards={activeCards}
-                            />
-                            <BillingAddressForm />
-                          </div>
-                        ) : (
-                          <div className="py-3">
-                            <TokenPaymentIcon
-                              className={`m-auto ${
-                                windowWidth < 1023 && windowWidth > 767
-                                  ? "-scale-x-100 w-1/2"
-                                  : "w-1/3"
-                              }`}
-                            />
-                            <p className="text-center mt-4">
-                              Click "Pay with {value}" to complete your
-                              purchase.
-                            </p>
-                          </div>
-                        )
-                      }
+                    <DisclosureRadioHeader
+                      className={`py-2 px-3 rounded-t-[0.5rem] ${
+                        isExpanded
+                          ? "bg-[#dbdbdb] border-b-2 border-b-brand-colour-5/30"
+                          : "text-brand-colour-5"
+                      }`}
+                      rightSlot={() => {
+                        const logoStyles = tv({
+                          base: "h-[1.25rem] w-auto",
+                        });
+                        const logoVariants: Variants = {
+                          normal: {
+                            filter: "grayscale(0%)",
+                            y: 0,
+                            opacity: 1,
+                          },
+                          grayedOut: {
+                            filter: "grayscale(100%)",
+                            y: 0.5,
+                            opacity: 0.6,
+                          },
+                        };
+                        if (value === "card") {
+                          return (
+                            <ul className="flex items-center gap-2">
+                              {allLogos.map((logo) => (
+                                <motion.li
+                                  key={logo}
+                                  variants={logoVariants}
+                                  initial="normal"
+                                  animate={
+                                    activeCards.includes(logo)
+                                      ? "normal"
+                                      : "grayedOut"
+                                  }
+                                >
+                                  {logo === "Visa" && (
+                                    <Visa className={logoStyles()} />
+                                  )}
+                                  {logo === "Mastercard" && (
+                                    <Mastercard className={logoStyles()} />
+                                  )}
+                                  {logo === "Amex" && (
+                                    <Amex className={logoStyles()} />
+                                  )}
+                                </motion.li>
+                              ))}
+                            </ul>
+                          );
+                        }
+                        if (value === "apple") {
+                          return <Apple className={logoStyles()} />;
+                        }
+                        if (value === "google") {
+                          return <Google className={logoStyles()} />;
+                        }
+                        if (value === "paypal") {
+                          return <Paypal className={logoStyles()} />;
+                        }
+                      }}
                     >
-                      <DisclosureRadioHeader
-                        className={`py-2 px-3 rounded-t-[0.5rem] ${
-                          isExpanded
-                            ? "bg-[#dbdbdb] border-b-2 border-b-brand-colour-5/30"
-                            : "text-brand-colour-5"
-                        }`}
-                        rightSlot={() => {
-                          const logoStyles = tv({
-                            base: "h-[1.25rem] w-auto",
-                          });
-                          const logoVariants: Variants = {
-                            normal: {
-                              filter: "grayscale(0%)",
-                              y: 0,
-                              opacity: 1,
-                            },
-                            grayedOut: {
-                              filter: "grayscale(100%)",
-                              y: 0.5,
-                              opacity: 0.6,
-                            },
-                          };
-                          if (value === "card") {
-                            return (
-                              <ul className="flex items-center gap-2">
-                                {allLogos.map((logo) => (
-                                  <motion.li
-                                    key={logo}
-                                    variants={logoVariants}
-                                    initial="normal"
-                                    animate={
-                                      activeCards.includes(logo)
-                                        ? "normal"
-                                        : "grayedOut"
-                                    }
-                                  >
-                                    {logo === "Visa" && (
-                                      <Visa className={logoStyles()} />
-                                    )}
-                                    {logo === "Mastercard" && (
-                                      <Mastercard className={logoStyles()} />
-                                    )}
-                                    {logo === "Amex" && (
-                                      <Amex className={logoStyles()} />
-                                    )}
-                                  </motion.li>
-                                ))}
-                              </ul>
-                            );
-                          }
-                          if (value === "apple") {
-                            return <Apple className={logoStyles()} />;
-                          }
-                          if (value === "google") {
-                            return <Google className={logoStyles()} />;
-                          }
-                          if (value === "paypal") {
-                            return <Paypal className={logoStyles()} />;
-                          }
-                        }}
-                      >
-                        {label}
-                      </DisclosureRadioHeader>
-                    </DisclosureRadio>
-                  </motion.div>
-                );
-              })}
-            </DisclosureRadioGroup>
-          </LayoutGroup>
+                      {label}
+                    </DisclosureRadioHeader>
+                  </DisclosureRadio>
+                </motion.div>
+              );
+            })}
+          </DisclosureRadioGroup>
         )}
       />
     </fieldset>
