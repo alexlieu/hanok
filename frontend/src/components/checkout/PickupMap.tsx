66f4241452467cc35e92fa77cfea33610c0aa219
alexlieu <alieu2001@googlemail.com>
2025-12-03 22:55:31 +0000

    feat(frontend/PickupMap): Add expandable fullscreen map to pickup map
    - Add a toggle button to switch between the inline and fixed (modal)
      positioning.
    - Add an animated fading background when the map is expanded to
      highlight the expanded map.
    - Add multiple methods to close an expanded map: toggling the expansion
      button, clicking on the background, or pressing 'Escape'.
    - Implement body scroll locking with padding adjustments - for the
      scroll bar - to prevent the background scrolling and layout shifts.
    - Add a placeholder wrapper to maintain document flow when the map is
      removed from the static grid (i.e. when it is expanded).
    - Trigger 'map.invalidateSize()' to ensure tiles render correctly
      (avoids greyed out map tiles appearing) immediately after expansion.
    
    Note: Avoided Motion's 'layout' prop for the expansion animation as it
    introduced visual warping of the map tiles during the expansion
    transition. An instant snap is used instead.

--- frontend/src/components/checkout/PickupMap.tsx
+++ frontend/src/components/checkout/PickupMap.tsx
@@ -2,15 +2,52 @@ import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { useEffect, useRef, useState } from "react";
 import L from "leaflet";
+import { Map as LeafletMap } from "leaflet";
 import { Button } from "../ui/aria/Button";
 import { LuPlus, LuMinus, LuCakeSlice } from "react-icons/lu";
 import { tv } from "tailwind-variants";
+import { AnimatePresence, motion } from "motion/react";
+import { IoMdExpand } from "react-icons/io";
 
 const position: [number, number] = [51.40313097396538, -0.2730678337183401];
 const maxZoom = 18;
 const minZoom = 10;
 
 const PickupMap: React.FC = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const mapRef = useRef<LeafletMap | null>(null);
+
+  useEffect(() => {
+    if (mapRef.current) {
+      mapRef.current.invalidateSize();
+    }
+  }, [isExpanded]);
+
+  useEffect(() => {
+    if (isExpanded) {
+      // What if there is no scrollbar?
+      const scrollbarWidth =
+        window.innerWidth - document.documentElement.clientWidth;
+      document.body.style.overflow = "hidden";
+      document.body.style.paddingRight = `${scrollbarWidth}px`;
+    } else {
+      document.body.style.overflow = "unset";
+      document.body.style.paddingRight = "unset";
+    }
+    return () => {
+      document.body.style.overflow = "unset";
+      document.body.style.paddingRight = "unset";
+    };
+  }, [isExpanded]);
+
+  useEffect(() => {
+    const handleEsc = (e: KeyboardEvent) => {
+      if (e.key === "Escape") setIsExpanded(false);
+    };
+    window.addEventListener("keydown", handleEsc);
+    return () => window.removeEventListener("keydown", handleEsc);
+  }, []);
+
   function MapControls() {
     const map = useMap();
     const [currentZoom, setCurrentZoom] = useState(map.getZoom());
@@ -56,59 +93,99 @@ const PickupMap: React.FC = () => {
     return (
       <div
         ref={controlsRef}
-        className="absolute top-2 right-2 w-10 h-30 z-1000 flex flex-col justify-evenly items-center"
+        className="absolute top-0 right-0 pt-2 pb-5 pr-2 w-12 h-full z-[1000] flex flex-col justify-between"
       >
+        <div className="w-full h-30 flex flex-col justify-evenly items-center">
+          <Button
+            variant="icon"
+            className={buttonStyling()}
+            onClick={() => map.setView(position, maxZoom, { animate: true })}
+            isDisabled={atOrigin}
+          >
+            <LuCakeSlice className="size-full" strokeWidth={2} />
+          </Button>
+          <Button
+            variant="icon"
+            className={buttonStyling()}
+            onClick={() => map.setZoom(map.getZoom() + 1)}
+            isDisabled={!canZoomIn}
+          >
+            <LuPlus className="size-full" strokeWidth={2} />
+          </Button>
+          <Button
+            variant="icon"
+            className={buttonStyling()}
+            onClick={() => map.setZoom(map.getZoom() - 1)}
+            isDisabled={!canZoomOut}
+          >
+            <LuMinus className="size-full" strokeWidth={2} />
+          </Button>
+        </div>
         <Button
           variant="icon"
-          className={buttonStyling}
-          onClick={() => map.setView(position, maxZoom, { animate: true })}
-          isDisabled={atOrigin}
+          className={buttonStyling()}
+          onClick={() => setIsExpanded((prevVal) => !prevVal)}
         >
-          <LuCakeSlice className="size-full" strokeWidth={2} />
-        </Button>
-        <Button
-          variant="icon"
-          className={buttonStyling}
-          onClick={() => map.setZoom(map.getZoom() + 1)}
-          isDisabled={!canZoomIn}
-        >
-          <LuPlus className="size-full" strokeWidth={2} />
-        </Button>
-        <Button
-          variant="icon"
-          className={buttonStyling}
-          onClick={() => map.setZoom(map.getZoom() - 1)}
-          isDisabled={!canZoomOut}
-        >
-          <LuMinus className="size-full" strokeWidth={2} />
+          <IoMdExpand className="size-full" strokeWidth={2} />
         </Button>
       </div>
     );
   }
 
+  const mapContainerStyle = tv({
+    base: "overflow-hidden bg-gray-100 shadow-sm",
+    variants: {
+      isExpanded: {
+        true: "fixed inset-0 z-50 h-[90%] w-[90%] m-auto",
+        false: "relative w-full aspect-3/2 sm:aspect-square mx-auto",
+      },
+    },
+  });
+
   return (
-    <div className="relative w-full aspect-3/2 sm:aspect-square mx-auto">
-      <MapContainer
-        center={position}
-        zoom={maxZoom}
-        minZoom={minZoom}
-        scrollWheelZoom={true}
-        zoomControl={false}
-        doubleClickZoom={false}
-        className="h-full w-full"
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={position}>
-          <Popup>
-            203 Kingston Rd, New Malden KT3 3SS <br /> Pickup your order here!{" "}
-          </Popup>
-        </Marker>
-        <MapControls />
-      </MapContainer>
-    </div>
+    <>
+      <AnimatePresence>
+        {isExpanded && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="fixed inset-0 bg-default-bg/5 z-40 backdrop-blur-xl"
+            onClick={() => setIsExpanded(false)}
+          />
+        )}
+      </AnimatePresence>
+      <div className={mapContainerStyle({ isExpanded: false })}>
+        <div
+          role={isExpanded ? "dialog" : undefined}
+          aria-modal={isExpanded ? "true" : undefined}
+          className={mapContainerStyle({ isExpanded })}
+        >
+          <MapContainer
+            ref={mapRef}
+            center={position}
+            zoom={maxZoom}
+            minZoom={minZoom}
+            scrollWheelZoom={true}
+            zoomControl={false}
+            doubleClickZoom={false}
+            className="h-full w-full"
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={position}>
+              <Popup>
+                203 Kingston Rd, New Malden KT3 3SS <br /> Pickup your order
+                here!{" "}
+              </Popup>
+            </Marker>
+            <MapControls />
+          </MapContainer>
+        </div>
+      </div>
+    </>
   );
 };
 
